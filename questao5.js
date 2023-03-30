91affe69ce31b9f51f4ddc89909823599f6b723c
Washington <washingtonldamacenac@gmail.com>
2023-03-30 17:23:03 -0300

    Update questao5.js

--- questao5.js
+++ questao5.js
@@ -1,7 +1,7 @@
 let string = "Socorram-me, subi no ônibus em Marrocos";
 let stringInvertida = "";
 
-for (let i = string.length - 1; i >= 0; i--) {
+for (let i = string.length -1; i >= 0; i--) {
   stringInvertida += string[i];
 }
 
